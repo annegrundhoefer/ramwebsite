3710815d7827ff278077698b19adc4a53888a436
Anne Grundhoefer <annegrundhoefer@gmail.com>
2015-03-14 20:25:59 -0500

    Initial Commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Dallas Window Coverings</title>
+<link rel="stylesheet" href="styles.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="//use.typekit.net/fgp2uiq.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<header>
+	<div class="container">
+		<div class="row"> 
+			<div class="col-sm-5 text-left">
+				<img src="http://placehold.it/200x120">
+			</div>	
+			<div class="col-sm-7 text-right menu">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About!</a></li>
+					<li><a href="#">Window Coverings</a></li>
+					<li><a href="#">Brands</a></li>
+					<li><a href="#">Get Quote</a></li>
+				</ul>
+			</div>	
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('header.php'); ?>
+
+<section id="mastHead">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7 col-sm-7 tagline">
+					<h1>xpert Design & Personal <br>Window Covering Solutions</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis, dui sit amet dapibus varius, metus massa elementum lectus, quis ultrices purus quam a urna.</p>
+			</div>
+			<div class="col-md-5 col-sm-5 quote">
+			</div>
+		</div>
+	</div>
+</section>
+
+</body>
+</html>
