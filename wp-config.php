8d2e3392df8a2eb3717603dbbf3a385345872811
Anne Grundhoefer <annegrundhoefer@gmail.com>
2015-03-18 16:09:12 -0500

    Update config file for server

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'ram_website');
+define('DB_NAME', 'ebgaines_ram');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'ebgaines_anne');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Nomnom21!');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
